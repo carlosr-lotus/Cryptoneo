10b9dc269cb1f29e23a7417ac7339b01e461a667
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2022-02-25 00:33:47 -0300

    Removed global userID props

--- src/components/dashboardSidebar.tsx
+++ src/components/dashboardSidebar.tsx
@@ -10,9 +10,7 @@ import { BsLightningChargeFill } from 'react-icons/bs';
 
 import styles from '../styles/components/DashboardSideBar.module.css';
 
-export default function DashboardSideBar(props) {
-    console.log(props.userID)
-    let getUserID = props.userID
+export default function DashboardSideBar() {
 
     function getCryptoPrice() {
         axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -29,20 +27,17 @@ export default function DashboardSideBar(props) {
 
             <div className={styles.sideBarAppTools}>
                 <Link href={{
-                    pathname: "/dashboard/currency",
-                    query: { userID: getUserID }
+                    pathname: "/dashboard/currency"
                 }}>
                     <IoStatsChart size={26} />
                 </Link>
                 <Link href={{
-                    pathname: "/dashboard/mining",
-                    query: { userID: getUserID }
+                    pathname: "/dashboard/mining"
                 }}>
                     <BsLightningChargeFill size={26} />
                 </Link>
                 <Link href={{
                     pathname: "/dashboard/wallet",
-                    query: { userID: getUserID }
                 }}>
                     <FaWallet size={26} />
                 </Link>
