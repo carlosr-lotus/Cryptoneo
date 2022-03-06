4edf3e3dbe5ff7577680e3558ae1da3b0aba4d7a
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2022-03-06 13:29:51 -0300

    Added logout button plus code adjusts

--- src/components/dashboardSidebar.tsx
+++ src/components/dashboardSidebar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import axios from 'axios';
 
 // Icons
@@ -6,15 +7,32 @@ import { FaUserAlt, FaWallet } from 'react-icons/fa';
 import { IoStatsChart } from 'react-icons/io5';
 import { IoIosSettings } from 'react-icons/io';
 import { BsLightningChargeFill } from 'react-icons/bs';
+import { BiLogOut } from 'react-icons/bi';
 // import { BiFullscreen } from 'react-icons/bi';
 
 import styles from '../styles/components/DashboardSideBar.module.css';
 
 export default function DashboardSideBar() {
 
+    const router = useRouter();
+
+    function userLogout() {
+
+        sessionStorage.removeItem('userID');
+        sessionStorage.removeItem('login');
+
+        router.push({
+            pathname: '/login'
+        })
+    }
+
     return (
         <div className={styles.sideBarContainer}>
-            <FaUserAlt size={21} />
+            <Link href={{
+                pathname: "/dashboard/profile"
+            }}>
+                <FaUserAlt size={21} />
+            </Link>
 
             <div className={styles.sideBarAppTools}>
                 <Link href={{
@@ -35,8 +53,7 @@ export default function DashboardSideBar() {
             </div>
 
             <div className={styles.sideBarAppSettings}>
-                <IoIosSettings size={26} />
-                {/* <BiFullscreen size={25} onClick={() => document.documentElement.requestFullscreen()} /> */}
+                <BiLogOut size={26} onClick={() => userLogout()} />
             </div>
         </div>
     )
